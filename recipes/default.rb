14c692470b38543d00758484590e484ebed48d4c
katsushi yamashita <yamakatun@gmail.com>
2012-12-21 18:52:07 +0900

    bug fix and add copyright message

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,19 @@
-
+# Author:: yamakatu(yamakatun@gmail.com)
+#
+# Copyright 2012 yamakatu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 if node['owner'] == 'root'
   home_dir = '/root/'
@@ -6,17 +21,16 @@ else
   home_dir = '/home/'+node['owner']+'/'
 end
 
-remote_directory home_dir << ".ssh/" do
+remote_directory home_dir + ".ssh/" do
   source "ssh"
   mode 0600
   owner node['owner']
   group node['owner']
 end
 
-cookbook_file home_dir << ".ssh/authorized_keys" do
+cookbook_file home_dir + ".ssh/authorized_keys" do
   source "ssh/authorized_keys"
   mode 0600
   owner node['owner']
   group node['owner']
 end
-
